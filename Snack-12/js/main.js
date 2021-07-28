576589076f78daccc4e423b3045c52d75b665282
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-07-28 12:13:16 +0200

    Affinamenti

--- Snack-12/js/main.js
+++ Snack-12/js/main.js
@@ -3,38 +3,6 @@
 var word1 = prompt('inserisci la prima parola');
 var word2 = prompt('inserisci la seconda parola');
 
-// function lunghezza(item1, item2) {
-//     if(item1.length > item2.length) {
-//         console.log(item1);
-//         document.getElementById('parola1').innerHTML = item1;
-//     }else if(item1.length < item2.length) {
-//         console.log(item2)
-//         document.getElementById('parola2').innerHTML = item2;
-//     }else {
-//         document.getElementById('parola1').innerHTML = item1;
-//         document.getElementById('parola2').innerHTML = item2;
-//         console.log(item1 + ' ' + item2);
-//     }
-// }
-
-// lunghezza(word1, word2);
-
-// function lunghezza(item1, item2) {
-//     if(item1.length > item2.length) {
-//         console.log(item1);
-//         return item1
-//     }else if(item1.length < item2.length) {
-//         console.log(item2)
-//         return item2
-//     }else {
-//         console.log(item1 + ' ' + item2);
-//         return item1, item2
-//     }
-// }
-
-// var parola = lunghezza(word1, word2);
-
-//CORREZIONE
 
 if (parolalunga(word1, word2)) {
     console.log(word1, word2);
@@ -59,9 +27,3 @@ function parolalunga (item1, item2) {
 
 
 
-// document.getElementById('parola1').innerHTML = item1;
-
-// document.getElementById('parola2').innerHTML = item2;
-
-// document.getElementById('parola1').innerHTML = item1;
-//         document.getElementById('parola2').innerHTML = item2;
